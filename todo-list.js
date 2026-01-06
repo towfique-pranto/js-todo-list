33b8cc46f1394660ad9c9fe2121979c0ca1abbba
Towfique Kabir <towfique.pranto@gmail.com>
2026-01-06 19:46:33 +0600

    loops, dom interactions

--- todo-list.js
+++ todo-list.js
@@ -1,4 +1,18 @@
 const todoList = [];
+renderTodoList()
+
+function renderTodoList(){
+let todoListHTML = '';
+
+for(let i=0;i<todoList.length;i++){
+  const todo = todoList[i];
+  const html = `<p>${todo}</p>`;
+  todoListHTML += html;
+}
+console.log(todoListHTML);
+
+document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value
@@ -6,4 +20,5 @@ function addTodo() {
   console.log(todoList);
 
   inputElement.value = '';
+  renderTodoList()
 }
