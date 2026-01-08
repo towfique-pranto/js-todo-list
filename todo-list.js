13d07c244b94268dd0a1c9faba29ec7281fa3f87
Towfique Kabir <towfique.pranto@gmail.com>
2026-01-09 01:07:17 +0600

    added delete button

--- todo-list.js
+++ todo-list.js
@@ -1,12 +1,12 @@
 const todoList = [];
-renderTodoList()
+renderTodoList();
 
 function renderTodoList(){
 let todoListHTML = '';
 
-for(let i=0;i<todoList.length;i++){
+for(let i=0; i<todoList.length; i++){
   const todo = todoList[i];
-  const html = `<p>${todo}</p>`;
+  const html = `<p>${todo} <button onclick="todoList.splice(${i}, 1); renderTodoList();">Delete</button></p>`;
   todoListHTML += html;
 }
 console.log(todoListHTML);
