6a57994027e02f9319dfb79913fe8b3fd077d58f
Towfique Kabir <towfique.pranto@gmail.com>
2026-01-13 00:20:32 +0600

    changed for loop to forEach method

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ renderTodoList();
 function renderTodoList(){
 let todoListHTML = '';
 
+todoList.forEach(function(todoObject, index){
+  const {name,dueDate} = todoObject 
+  const html = `
+  <div>${name}</div>
+  <div>${dueDate}</div>
+  <button onclick="todoList.splice(${index}, 1); renderTodoList();"
+    class="delete-button">Delete</button>`;
+  todoListHTML += html;
+});
+/*
 for(let i=0; i<todoList.length; i++){
   const todoObject = todoList[i];
   const {name,dueDate} = todoObject //const name = todoObject.name; // const dueDate = todoObject.dueDate;
@@ -14,7 +24,7 @@ for(let i=0; i<todoList.length; i++){
   <button onclick="todoList.splice(${i}, 1); renderTodoList();"
     class="delete-button">Delete</button>`;
   todoListHTML += html;
-}
+}*/
 //console.log(todoListHTML);
 
 document.querySelector('.js-todo-list').innerHTML = todoListHTML;
