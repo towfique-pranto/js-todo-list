90adf83f3ba29abe2f78cc66d5790df9ed9e4c2e
Towfique Kabir <towfique.pranto@gmail.com>
2026-01-14 04:17:46 +0600

    changed a normal function to arrow function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ renderTodoList();
 function renderTodoList(){
 let todoListHTML = '';
 
-todoList.forEach(function(todoObject, index){
+todoList.forEach((todoObject, index)=>{
   const {name,dueDate} = todoObject 
   const html = `
   <div>${name}</div>
