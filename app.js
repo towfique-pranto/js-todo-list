8eda9db6de8abaf5ffc4310a6743a5acefc3aa67
Towfique Kabir <towfique.pranto@gmail.com>
2026-01-14 21:32:57 +0600

    replaced onclick with event listeners

--- app.js
+++ app.js
@@ -9,12 +9,24 @@ todoList.forEach((todoObject, index)=>{
   const html = `
   <div>${name}</div>
   <div>${dueDate}</div>
-  <button onclick="todoList.splice(${index}, 1); renderTodoList();"
-    class="delete-button">Delete</button>`;
+  <button onclick=""
+    class="delete-button js-delete-button">Delete</button>`;
   todoListHTML += html;
 });
 document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+document.querySelectorAll('.js-delete-button').forEach((deleteButton, index)=>{
+  deleteButton.addEventListener('click',()=>{
+    todoList.splice(index, 1);
+    renderTodoList();
+  });
+});
 }
+
+document.querySelector('.js-add-button').addEventListener('click',()=>{
+  addTodo();
+});
+
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const inputDateElement = document.querySelector('.js-duedate-input');
